661af9fb3e75727962569906f92cf426aff8363f
Sabit Siraji <sabitsiraji@gmail.com>
2025-01-20 09:51:22 +0600

    AT LAST SEGMENT TREE BUILD KORTE PARSI!! tao visualize koraite partesi na :(

--- src/main/java/com/algo/SegmentTreeVisualizationController.java
+++ src/main/java/com/algo/SegmentTreeVisualizationController.java
@@ -1,5 +1,6 @@
 package com.algo;
 
+import javafx.util.Pair;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -16,6 +17,21 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+class Segment_Tree_Nodes {
+    int value;
+    double x, y;
+
+    Segment_Tree_Nodes() {
+
+    }
+
+    Segment_Tree_Nodes(int value, double x, double y) {
+        this.value = value;
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class SegmentTreeVisualizationController extends Application {
 
     @FXML
@@ -27,7 +43,7 @@ public class SegmentTreeVisualizationController extends Application {
 
     int N = 100000;
     int[] numbers = new int[N];
-    int[] segment_tree = new int[4 * N];
+    Segment_Tree_Nodes[] segment_tree = new Segment_Tree_Nodes[4 * N];
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -39,17 +55,25 @@ public class SegmentTreeVisualizationController extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SegmentTreeVisualizationController.class.getResource("/com/algo/" + fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                SegmentTreeVisualizationController.class.getResource("/com/algo/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
     @FXML
     public void initialize() {
         gc = canvas.getGraphicsContext2D();
+        // Initialize the segment_tree array with Segment_Tree_Nodes instances
+        for (int i = 0; i < segment_tree.length; i++) {
+            segment_tree[i] = new Segment_Tree_Nodes();
+        }
     }
 
     @FXML
     private void HandleBuild() {
+        // Clear the canvas
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
         String input = BuildInput.getText();
         if (!input.matches("[0-9 ]+")) {
             Alert alert = new Alert(AlertType.ERROR);
@@ -67,7 +91,9 @@ public class SegmentTreeVisualizationController extends Application {
         }
 
         double canvas_width = canvas.getWidth();
-        build_segment_tree(1, 0, numbers.length - 1, canvas_width / 2, canvas_width);
+        build_segment_tree(1, 0, numbers.length - 1, 0, canvas_width);
+        build_lines(1, 0, numbers.length - 1);
+        build_circle(1, 0, numbers.length - 1);
     }
 
     @FXML
@@ -82,39 +108,90 @@ public class SegmentTreeVisualizationController extends Application {
 
     void build_segment_tree(int node, int start, int end, double canvas_start_point, double canvas_width) {
         if (start == end) {
-            form_node(node, start, canvas_start_point, canvas_width);
+            segment_tree[node].value = numbers[start];
         } else {
             int mid = (start + end) / 2;
             build_segment_tree(2 * node, start, mid, canvas_start_point, (canvas_start_point + canvas_width) / 2);
             build_segment_tree(2 * node + 1, mid + 1, end, (canvas_start_point + canvas_width) / 2, canvas_width);
-            segment_tree[node] = segment_tree[2 * node] + segment_tree[2 * node + 1];
+            segment_tree[node].value = segment_tree[2 * node].value + segment_tree[2 * node + 1].value;
         }
+        System.out.println("Building tree: node=" + node + ", range=[" + start + ", " + end + "]");
+        // for(int i=0;i<100000L;i++)
+        build_helper(node, canvas_start_point, canvas_width);
     }
 
-    void form_node(int node, int start, double canvas_start_point, double canvas_width) {
-        segment_tree[node] = numbers[start];
-        double width_point = (canvas_start_point + canvas_width) / 2;
+    void build_helper(int node, double canvas_start_point, double canvas_width) {
         double tree_height = Math.ceil(Math.log(numbers.length) / Math.log(2));
-        double height_index = canvas.getHeight() / tree_height;
+        double width_point = (canvas_start_point + canvas_width) / 2;
+        int level = (int) (Math.log(node) / Math.log(2)); // Determine the level of the node
+        double y = level * (canvas.getHeight() / tree_height) * 0.5 + 50; // Adjust y position
+        segment_tree[node].x = width_point - 15;
+        segment_tree[node].y = y - 15;
 
-        new Thread(() -> {
-            for (double opacity = 0.0; opacity <= 1.0; opacity += 0.1) {
-                final double currentOpacity = opacity;
-                javafx.application.Platform.runLater(() -> {
-                    gc.setGlobalAlpha(currentOpacity);
-                    gc.setFill(Color.BLUE);
-                    gc.fillOval(width_point - 10, height_index * node - 10, 20, 20); // Draw a circle with radius 10
-                    gc.setFill(Color.GREY);
-                    gc.fillText(String.valueOf(segment_tree[node]), width_point - 5, height_index * node + 5); // Draw the value inside the circle
-                });
-                try {
-                    Thread.sleep(100); // Delay for animation effect
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            Platform.runLater(() -> gc.setGlobalAlpha(1.0)); // Reset opacity to 1.0
-        }).start();
+        // Platform.runLater(() -> {
+        // gc.setGlobalAlpha(1.0);
+        // gc.setFill(Color.GRAY);
+        // gc.fillOval(width_point - 15, y - 15, 30, 30); // Adjust circle position and
+        // increase radius
+        // gc.setFill(Color.WHITE);
+        // gc.fillText(String.valueOf(segment_tree[node].value), width_point - 5, y +
+        // 5); // Draw the value
+        // });
+    }
+
+    Pair<Double, Double> build_lines(int node, int start, int end) {
+        if (start == end) {
+            //build_helper(segment_tree[node].x, segment_tree[node].y, segment_tree[node].value, node);
+            return new Pair<>(segment_tree[node].x, segment_tree[node].y);
+        }
+
+        int mid = (start + end) / 2;
+        Pair<Double, Double> left = build_lines(2 * node, start, mid);
+        Pair<Double, Double> right = build_lines(2 * node + 1, mid + 1, end);
+
+        double x = segment_tree[node].x;
+        double y = segment_tree[node].y;
+
+        build_helper(x, y, left.getKey(), left.getValue());
+        build_helper(x, y, right.getKey(), right.getValue());
+
+        return new Pair<>(x, y);
+    }
+
+    void build_circle(int node, int start, int end) {
+        if (start == end) {
+            build_helper(segment_tree[node].x, segment_tree[node].y, segment_tree[node].value, node);
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        build_circle(2 * node, start, mid);
+        build_circle(2 * node + 1, mid + 1, end);
+
+        double x = segment_tree[node].x;
+        double y = segment_tree[node].y;
+
+        build_helper(x, y, segment_tree[node].value, node);
+    }
+
+    void build_helper(double x, double y, int value, int node) {
+        Platform.runLater(() -> {
+            gc.setGlobalAlpha(1.0);
+            gc.setFill(Color.GRAY);
+            gc.fillOval(x - 15, y - 15, 50, 50); // Adjust circle position and increase radius
+            gc.setFill(Color.WHITE);
+            gc.fillText(String.valueOf(value), x + 3 , y + 6); // Draw the value
+            gc.setFill(Color.BLACK);
+            gc.fillText(String.valueOf(node), x + 3, y + 46); // Draw the value
+        });
+    }
+
+    void build_helper(double x1, double y1, double x2, double y2) {
+        Platform.runLater(() -> {
+            gc.setStroke(Color.BLACK);
+            gc.setLineWidth(2);
+            gc.strokeLine(x1, y1, x2, y2);
+        });
     }
 
     public static void main(String[] args) {
