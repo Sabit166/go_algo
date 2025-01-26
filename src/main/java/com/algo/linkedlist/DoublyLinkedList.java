5b2ab163f411814dd540fc5803e1a59208271246
Sabit Siraji <sabitsiraji@gmail.com>
2025-01-26 15:59:01 +0600

    Implement DoublyLinkedList and SinglyLinkedList with basic operations

--- src/main/java/com/algo/linkedlist/DoublyLinkedList.java
+++ src/main/java/com/algo/linkedlist/DoublyLinkedList.java
@@ -1,5 +1,127 @@
 package com.algo.linkedlist;
 
 public class DoublyLinkedList {
-    
+    private doublyNode head;
+
+    public DoublyLinkedList() {
+        this.head = null;
+    }
+
+    // Push to the front
+    public void pushFront(int value, double x, double y) {
+        doublyNode newNode = new doublyNode(value, x, y);
+        newNode.setNext(head);
+        if (head != null) {
+            head.setPrev(newNode);
+        }
+        head = newNode;
+    }
+
+    // Push to the back
+    public void pushBack(int value, double x, double y) {
+        doublyNode newNode = new doublyNode(value, x, y);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        doublyNode temp = head;
+        while (temp.getNext() != null) {
+            temp = temp.getNext();
+        }
+        temp.setNext(newNode);
+        newNode.setPrev(temp);
+    }
+
+    // Pop from the front
+    public void popFront() {
+        if (head != null) {
+            doublyNode temp = head;
+            head = head.getNext();
+            if (head != null) {
+                head.setPrev(null);
+            }
+            temp = null;
+        }
+    }
+
+    // Pop from the back
+    public void popBack() {
+        if (head == null) {
+            return;
+        }
+        if (head.getNext() == null) {
+            head = null;
+            return;
+        }
+        doublyNode temp = head;
+        while (temp.getNext().getNext() != null) {
+            temp = temp.getNext();
+        }
+        temp.setNext(null);
+    }
+
+    // Print the list
+    public void printList() {
+        doublyNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.getValue() + " ");
+            temp = temp.getNext();
+        }
+        System.out.println();
+    }
+}
+
+class doublyNode {
+    private int value;
+    private double x, y;
+    private doublyNode next;
+    private doublyNode prev;
+
+    public doublyNode(int value, double x, double y) {
+        this.value = value;
+        this.x = x;
+        this.y = y;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public doublyNode getNext() {
+        return next;
+    }
+
+    public void setNext(doublyNode next) {
+        this.next = next;
+    }
+
+    public doublyNode getPrev() {
+        return prev;
+    }
+
+    public void setPrev(doublyNode prev) {
+        this.prev = prev;
+    }
 }
--- src/main/java/com/algo/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/algo/linkedlist/SinglyLinkedList.java
@@ -1,5 +1,145 @@
 package com.algo.linkedlist;
 
 public class SinglyLinkedList {
-    
+    private SinglyNode head;
+
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+
+    // Insert at the end
+    public void insertAtEnd(int value, double x, double y) {
+        SinglyNode newNode = new SinglyNode(value, x, y);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        SinglyNode temp = head;
+        while (temp.getNext() != null) {
+            temp = temp.getNext();
+        }
+        temp.setNext(newNode);
+    }
+
+    // Pop from the front
+    public void popFront() {
+        if (head != null) {
+            SinglyNode temp = head;
+            head = head.getNext();
+            temp = null;
+        }
+    }
+
+    // Pop from the back
+    public void popBack() {
+        if (head == null) {
+            return;
+        }
+        if (head.getNext() == null) {
+            head = null;
+            return;
+        }
+        SinglyNode temp = head;
+        while (temp.getNext().getNext() != null) {
+            temp = temp.getNext();
+        }
+        temp.setNext(null);
+    }
+
+    // Insert at a specific position
+    public void insertAt(int index, int value, double x, double y) {
+        SinglyNode newNode = new SinglyNode(value, x, y);
+        if (index == 0) {
+            newNode.setNext(head);
+            head = newNode;
+            return;
+        }
+        SinglyNode temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            if (temp.getNext() == null) {
+                return;
+            }
+            temp = temp.getNext();
+        }
+        newNode.setNext(temp.getNext());
+        temp.setNext(newNode);
+    }
+
+    // Delete at a specific position
+    public void deleteAt(int index) {
+        if (head == null) {
+            return;
+        }
+        if (index == 0) {
+            SinglyNode temp = head;
+            head = head.getNext();
+            temp = null;
+            return;
+        }
+        SinglyNode temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            if (temp.getNext() == null) {
+                return;
+            }
+            temp = temp.getNext();
+        }
+        SinglyNode delNode = temp.getNext();
+        temp.setNext(delNode.getNext());
+        delNode = null;
+    }
+
+    // Print the list
+    public void printList() {
+        SinglyNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.getValue() + " ");
+            temp = temp.getNext();
+        }
+        System.out.println();
+    }
+}
+
+class SinglyNode {
+    protected int value;
+    private SinglyNode next;
+    protected double x;
+    protected double y;
+
+    public SinglyNode() {
+        this.next = null;
+        this.value = 0;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public SinglyNode(int value, double x, double y) {
+        this.value = value;
+        this.x = x;
+        this.y = y;
+        this.next = null;
+    }
+
+    public SinglyNode getNext() {
+        return next;
+    }
+
+    public void setNext(SinglyNode next) {
+        this.next = next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
