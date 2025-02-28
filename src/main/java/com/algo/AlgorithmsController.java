74ff0575218791d01dafbd04990193de8df22107
Sabit Siraji <sabitsiraji@gmail.com>
2025-02-28 15:27:31 +0600

    Enable full-screen mode for the algorithm visualization window

--- src/main/java/com/algo/AlgorithmsController.java
+++ src/main/java/com/algo/AlgorithmsController.java
@@ -39,8 +39,8 @@ public class AlgorithmsController {
         Parent root = FXMLLoader.load(getClass().getResource("/com/algo/" + fxml + ".fxml"));
         Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
-        stage.setFullScreen(true);
         stage.setScene(scene);
+        stage.setFullScreen(true);
         stage.setTitle(fxml.replace("_", " ").toUpperCase());
         stage.show();
     }
